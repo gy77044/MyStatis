0aa273692a9a1b1983170c8823b0e738f3ce5c79
gautam yadav <gy77044@gmail.com>
2021-07-22 16:07:05 +0530

    donne
    ds

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function showFormat() {
-  var x = document.getElementById("enter-number").value;   
+  document.getElementById("enter-number").value;   
         document.getElementById("small").innerHTML = "Enter Digits Only";
 }
+
+
